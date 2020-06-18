69958192e149820e12a5a06f7df66a64213e29b2
seb <seb.hallx@gmail.com>
2020-06-18 13:33:41 +0100

    added player

--- maze.py
+++ maze.py
@@ -1,6 +1,22 @@
 import pygame
 import sys
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((30, 30))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect(center=(100, 100))
+
+    def update(self):
+        pass
+
+
+player1 = Player()
+player_group = pygame.sprite.Group()
+player_group.add(player1)
+
 pygame.init()
 clock = pygame.time.Clock()
 screen_width, screen_height = 800, 800
@@ -14,7 +30,9 @@ while running:
             pygame.quit()
             sys.exit()
 
-    screen.fill((30, 30, 30))
+    screen.fill((255, 255, 255))
+    player_group.draw(screen)
+    player_group.update()
 
     pygame.display.flip()
     clock.tick(60)
