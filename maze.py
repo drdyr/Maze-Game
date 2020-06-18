028e2939514e510f904cde98ec55f5c76f029b56
Finn <finnbutler1@gmail.com>
2020-06-18 14:42:07 +0100

    adding gen algorithm

--- maze.py
+++ maze.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 import sys
 
@@ -13,21 +14,58 @@ class Player(pygame.sprite.Sprite):
         pass
 
 
-class Maze():
+class Maze:
     def __init__(self, width, height):
+        self.cells = []
+        self.odd_cells = []
+        self.width = width
+        self.height = height
         for x in range(width):
             for y in range(height):
-                rect = pygame.Rect((screen_width / width) * x, (screen_height / height) * y, screen_width / width, screen_height / height)
-                pygame.draw.rect(screen, (200, 200, 200), rect, 0)
+                cell = Cell(x, y, WALL, (screen_width / width), (screen_height / height))
+                if (x != 0 and x != width - 1) and (y != 0 and y != height - 1):
+                    self.cells.append(cell)
+                    if x % 2 == 1 and y % 2 == 1:
+                        self.odd_cells.append(cell)
 
-class Cell():
-    def __init(self, x, y, type):
+    def generate(self):
+        cells = self.odd_cells
+        stack = []
+        start_cell = random.choice(cells)
+        stack.append(start_cell)
+        while len(stack) != 0:
+            neighbour = random.choice(get_neighbours(start_cell))
+
+    def get_neighbours(self, cell):
+        x = cell.x
+        y = cell.y
+        neighbours = []
+        for i in range(-2, 2, 4):
+            # add is unvisited
+            if Cell(x + i, y, WALL, (screen_width / self.width), (screen_height / self.height)) in self.odd_cells:
+                neighbours.append(Cell(x + i, y, WALL, (screen_width / self.width), (screen_height / self.height)))
+            if Cell(x, y + i, WALL, (screen_width / self.width), (screen_height / self.height)) in self.odd_cells:
+                neighbours.append(Cell(x, y + i, WALL, (screen_width / self.width), (screen_height / self.height)))
+
+        return neighbours
+
+
+class Cell:
+    def __init__(self, x, y, cell_type, width, height):
         self.x = x
         self.y = y
-        self.type = WALL
+        self.cell_type = cell_type
+        self.rect = pygame.Rect(width * x, height * y, width,
+                                height)
+        pygame.draw.rect(screen, cell_type, self.rect, 0)
 
-PASSAGE = 1
-WALL = 0
+    def set_type(self, cell_type):
+        self.cell_type = cell_type
+        pygame.draw.rect(screen, cell_type, self.rect, 0)
+
+
+PASSAGE = (255, 255, 255)
+WALL = (170, 170, 170)
 
 player1 = Player()
 player_group = pygame.sprite.Group()
